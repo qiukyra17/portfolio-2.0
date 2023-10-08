ae3b07e53f616cd6e0ccdaa90dce26046ef3ac44
qiukyra17 <kyraqiu@gmail.com>
2023-10-08 18:45:52 -0400

    small screen adjustments

---
+++ Adjustment.js
@@ -0,0 +1,18 @@
+const firstSet = document.getElementById("firstSet");
+const secondSet = document.getElementById("secondSet");
+const smallMainMenu = document.getElementById("smallMainMenu");
+
+
+let screenWidth = window.innerWidth;
+console.log("Screen width: " + screenWidth); // Debugging line
+
+function rowOrCol() {
+    if (screenWidth >= 391) {
+        firstSet.className = "row";
+        secondSet.className = "row";
+    } else {
+        smallMainMenu.className = "row";
+    }
+}
+
+rowOrCol();
